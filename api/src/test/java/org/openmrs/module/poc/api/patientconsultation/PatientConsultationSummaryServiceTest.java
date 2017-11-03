34ecfcb16c3e650da07850bd746e656a0d9ee32b
Guimino <minoneves@gmail.com>
2017-11-03 11:08:17 +0200

    Created Unit Tests for service PatientConsultationSummaryService

--- api/src/test/java/org/openmrs/module/poc/api/patientconsultation/PatientConsultationSummaryServiceTest.java
+++ api/src/test/java/org/openmrs/module/poc/api/patientconsultation/PatientConsultationSummaryServiceTest.java
@@ -1,12 +1,3 @@
-/**
- * This Source Code Form is subject to the terms of the Mozilla Public License,
- * v. 2.0. If a copy of the MPL was not distributed with this file, You can
- * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
- * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
- *
- * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
- * graphic logo is a trademark of OpenMRS Inc.
- */
 package org.openmrs.module.poc.api.patientconsultation;
 
 import java.util.Calendar;
@@ -25,7 +16,8 @@ import org.openmrs.module.poc.patientconsultation.model.PatientConsultationSumma
 import org.openmrs.module.poc.patientconsultation.service.PatientConsultationSummaryService;
 
 public class PatientConsultationSummaryServiceTest extends POCBaseModuleContextSensitiveTest {
-	
+
+	@SuppressWarnings("unchecked")
 	@Test
 	public void shouldFindPatientConsultationsByLocationAndDateInterval() throws Exception {
 		this.executeDataSet("patientconsultation/shouldFindPatientConsultationsByLocationAndDateInterval-dataset.xml");
@@ -34,20 +26,20 @@ public class PatientConsultationSummaryServiceTest extends POCBaseModuleContextS
 		calendar.set(Calendar.MONTH, 9);
 		calendar.set(Calendar.DAY_OF_MONTH, 20);
 		final Date startDate = calendar.getTime();
-		
+
 		calendar.set(Calendar.YEAR, 2017);
 		calendar.set(Calendar.MONTH, 10);
 		calendar.set(Calendar.DAY_OF_MONTH, 3);
 		final Date endDate = calendar.getTime();
-		
+
 		final List<PatientConsultationSummary> patientSummaries = Context
-		        .getService(PatientConsultationSummaryService.class)
-		        .findPatientConsultationsByLocationAndDateInterval(new Location(2), startDate, endDate);
-		
+				.getService(PatientConsultationSummaryService.class)
+				.findPatientConsultationsByLocationAndDateInterval(new Location(2), startDate, endDate);
+
 		MatcherAssert.assertThat(patientSummaries, CoreMatchers.notNullValue());
 		MatcherAssert.assertThat(patientSummaries, IsCollectionWithSize.hasSize(3));
 		MatcherAssert.assertThat(patientSummaries, CoreMatchers.hasItems(Matchers
-		        .<PatientConsultationSummary> hasProperty("patientConsultations", IsCollectionWithSize.hasSize(1))));
-		
+				.<PatientConsultationSummary>hasProperty("patientConsultations", IsCollectionWithSize.hasSize(1))));
+
 	}
 }
