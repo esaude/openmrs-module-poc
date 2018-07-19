0358f521ff34b7941319f4415fc581b16d8b2050
Guimino <minoneves@gmail.com>
2018-07-19 10:03:58 +0200

    EPCT-15 System is displaying previously updated values (#15)
    * added new service for updating clinical services

--- api/src/main/java/org/openmrs/module/poc/clinicalservice/service/ClinicalServiceServiceImpl.java
+++ api/src/main/java/org/openmrs/module/poc/clinicalservice/service/ClinicalServiceServiceImpl.java
@@ -9,14 +9,21 @@
  */
 package org.openmrs.module.poc.clinicalservice.service;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.openmrs.Concept;
+import org.openmrs.ConceptDatatype;
 import org.openmrs.Encounter;
 import org.openmrs.Obs;
+import org.openmrs.api.APIException;
 import org.openmrs.api.EncounterService;
 import org.openmrs.api.ObsService;
 import org.openmrs.api.context.Context;
@@ -24,7 +31,10 @@ import org.openmrs.api.impl.BaseOpenmrsService;
 import org.openmrs.module.poc.api.common.exception.POCBusinessException;
 import org.openmrs.module.poc.clinicalservice.util.ClinicalServiceKeys;
 import org.openmrs.module.poc.clinicalservice.util.ClinicalServiceUtil;
+import org.openmrs.module.poc.clinicalservice.util.ConceptUUIDConstants;
+import org.openmrs.module.poc.clinicalservice.util.MappedClinicalServices;
 import org.openmrs.module.poc.clinicalservice.validation.ClinicalServiceValidator;
+import org.openmrs.module.poc.pocheuristic.service.PocHeuristicService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class ClinicalServiceServiceImpl extends BaseOpenmrsService implements ClinicalServiceService {
@@ -38,6 +48,14 @@ public class ClinicalServiceServiceImpl extends BaseOpenmrsService implements Cl
 	
 	@Autowired
 	private ClinicalServiceUtil clinicalServiceUtil;
+	
+	private static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm";
+	
+	private static final String DATE_PATTERN = "yyyy-MM-dd";
+	
+	private static final List<String> EXCLUDING_CONCEPTS = Arrays.asList(ConceptUUIDConstants.POC_MAPPING_WHO_DATE,
+	    ConceptUUIDConstants.POC_MAPPING_ANAMNESE_DATE, ConceptUUIDConstants.POC_MAPPING_DIAGNOSIS_DATE,
+	    ConceptUUIDConstants.POC_MAPPING_VITALS_DATE);
 	
 	@Override
 	public void deleteClinicalService(final String encounterUuid, final String clinicalServiceKey)
@@ -111,5 +129,140 @@ public class ClinicalServiceServiceImpl extends BaseOpenmrsService implements Cl
 	@Override
 	public void setEncounterService(final EncounterService encounterService) {
 		this.encounterService = encounterService;
+	}
+	
+	@Override
+	public Encounter updateClinicalService(final String serviceCode, final Encounter encounter,
+	        final List<Map<String, Object>> lstMapNewObs) {
+		
+		final Encounter fetchedEncounter = this.encounterService.getEncounterByUuid(encounter.getUuid());
+		
+		if ((serviceCode != null) && (fetchedEncounter != null)) {
+			
+			this.deleteOldObsOfClinicalService(fetchedEncounter, serviceCode);
+			this.createNewObsForClinicalService(fetchedEncounter, lstMapNewObs);
+			return this.encounterService.saveEncounter(fetchedEncounter);
+		}
+		throw new APIException(Context.getMessageSourceService().getMessage(
+		    "poc.error.cannot.update.details.forgiven.service.and.encounter",
+		    new String[] { serviceCode, (fetchedEncounter != null) ? fetchedEncounter.getUuid() : null },
+		    Context.getLocale()));
+	}
+	
+	private void deleteOldObsOfClinicalService(final Encounter fetchedEncounter, final String serviceCode) {
+		
+		final List<String> clinicalServices = MappedClinicalServices
+		        .getClinicalServices(ClinicalServiceKeys.getClinicalServiceByCode(serviceCode));
+		
+		final Set<Obs> allOldObs = fetchedEncounter.getAllObs(false);
+		final Set<Obs> obsGroups = new HashSet<>();
+		for (final Obs oldObsItem : allOldObs) {
+			
+			final String conceptUuid = oldObsItem.getConcept().getUuid();
+			if ((oldObsItem.getObsId() != null) && clinicalServices.contains(conceptUuid)
+			        && !ClinicalServiceServiceImpl.EXCLUDING_CONCEPTS.contains(conceptUuid)) {
+				this.voidObs(oldObsItem);
+				
+				if (oldObsItem.getObsGroup() != null) {
+					obsGroups.add(oldObsItem.getObsGroup());
+				}
+			}
+		}
+		
+		// voiding ObsGroups without active membes
+		for (final Obs obs : obsGroups) {
+			if (!obs.hasGroupMembers(false)) {
+				this.voidObs(obs);
+			}
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void createNewObsForClinicalService(final Encounter encounter,
+	        final List<Map<String, Object>> lstMapNewObs) {
+		
+		try {
+			for (final Map<String, Object> obsMap : lstMapNewObs) {
+				
+				Concept conceptGroup = null;
+				Concept concept = null;
+				Object value = null;
+				
+				final List<Map<String, Object>> groupMembers = (List<Map<String, Object>>) obsMap.get("groupMembers");
+				
+				if (groupMembers != null) {
+					
+					conceptGroup = Context.getConceptService().getConceptByUuid((String) obsMap.get("concept"));
+					
+					for (final Map<String, Object> mapGroupMember : groupMembers) {
+						
+						concept = Context.getConceptService().getConceptByUuid((String) mapGroupMember.get("concept"));
+						value = mapGroupMember.get("value");
+						this.createObsWithGroupObs(encounter, concept, conceptGroup, value);
+					}
+					
+				} else {
+					concept = Context.getConceptService().getConceptByUuid((String) obsMap.get("concept"));
+					value = obsMap.get("value");
+					encounter.addObs(this.createOnlyObs(encounter, concept, value));
+				}
+			}
+		}
+		catch (final ParseException e) {
+			throw new APIException(e);
+		}
+	}
+	
+	private void createObsWithGroupObs(final Encounter encounter, final Concept concept, final Concept conceptGroup,
+	        final Object value) throws ParseException {
+		
+		Obs obsGroup = Context.getService(PocHeuristicService.class).findObsByEncounterAndConcept(encounter,
+		    conceptGroup);
+		if (obsGroup == null) {
+			obsGroup = new Obs();
+			obsGroup.setConcept(conceptGroup);
+			obsGroup.setPerson(encounter.getPatient());
+		}
+		final Obs obsMember = this.createOnlyObs(encounter, concept, value);
+		obsGroup.addGroupMember(obsMember);
+		encounter.addObs(obsGroup);
+		this.encounterService.saveEncounter(encounter);
+	}
+	
+	private Obs createOnlyObs(final Encounter encounter, final Concept concept, final Object value)
+	        throws ParseException {
+		
+		final Obs obs = new Obs();
+		obs.setConcept(concept);
+		obs.setEncounter(encounter);
+		this.setObsValue(obs, concept, value);
+		obs.setPerson(encounter.getPatient());
+		return obs;
+	}
+	
+	private void voidObs(final Obs obs) {
+		this.obsService.voidObs(obs, Context.getMessageSourceService().getMessage("poc.global.info.voided.forupdate"));
+	}
+	
+	private void setObsValue(final Obs obs, final Concept concept, final Object value) throws ParseException {
+		
+		final ConceptDatatype datatype = concept.getDatatype();
+		if (datatype.isNumeric()) {
+			obs.setValueNumeric(Double.valueOf(value.toString()));
+		} else if (datatype.isBoolean()) {
+			obs.setValueBoolean(Boolean.valueOf(value.toString()));
+		} else if (datatype.isDate()) {
+			final DateFormat dateFormat = new SimpleDateFormat(ClinicalServiceServiceImpl.DATE_PATTERN);
+			obs.setValueDate(dateFormat.parse(value.toString()));
+		} else if (datatype.isDateTime()) {
+			final DateFormat dateFormat = new SimpleDateFormat(ClinicalServiceServiceImpl.DATE_TIME_PATTERN);
+			obs.setValueDatetime(dateFormat.parse(value.toString()));
+		} else if (datatype.isCoded()) {
+			obs.setValueCoded(Context.getConceptService().getConceptByUuid(value.toString()));
+		} else if (datatype.isText()) {
+			obs.setValueText(value.toString());
+		} else {
+			throw new RuntimeException("Don't know how to handle " + datatype);
+		}
 	}
 }
--- omod/src/main/java/org/openmrs/module/poc/web/resource/ClinicalServiceResource.java
+++ omod/src/main/java/org/openmrs/module/poc/web/resource/ClinicalServiceResource.java
@@ -12,24 +12,30 @@
  */
 package org.openmrs.module.poc.web.resource;
 
+import java.util.List;
+import java.util.Map;
+
 import org.openmrs.Encounter;
 import org.openmrs.api.APIException;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.poc.api.common.exception.POCBusinessException;
 import org.openmrs.module.poc.clinicalservice.service.ClinicalServiceService;
+import org.openmrs.module.webservices.rest.SimpleObject;
 import org.openmrs.module.webservices.rest.web.RequestContext;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
 import org.openmrs.module.webservices.rest.web.resource.api.PageableResult;
 import org.openmrs.module.webservices.rest.web.resource.impl.EmptySearchResult;
+import org.openmrs.module.webservices.rest.web.response.ResponseException;
 import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_9.EncounterResource1_9;
 
 /**
  *
  */
 
-@Resource(name = RestConstants.VERSION_1 + "/clinicalservice", order = 2, supportedClass = Encounter.class, supportedOpenmrsVersions = {
-        "1.11.*", "1.12.*" })
+@Resource(name = RestConstants.VERSION_1
+        + "/clinicalservice", order = 2, supportedClass = Encounter.class, supportedOpenmrsVersions = { "1.11.*",
+        "1.12.*" })
 public class ClinicalServiceResource extends EncounterResource1_9 {
 	
 	@Override
@@ -42,5 +48,15 @@ public class ClinicalServiceResource extends EncounterResource1_9 {
 		catch (final POCBusinessException e) {
 			throw new APIException(e);
 		}
+	}
+	
+	@Override
+	public Encounter update(final String uuid, final SimpleObject propertiesToUpdate, final RequestContext context)
+	        throws ResponseException {
+		
+		final Encounter delegate = this.getByUniqueId(uuid);
+		final String serviceCode = (String) propertiesToUpdate.remove("serviceCode");
+		final List<Map<String, Object>> lstMapObs = propertiesToUpdate.get("obs");
+		return Context.getService(ClinicalServiceService.class).updateClinicalService(serviceCode, delegate, lstMapObs);
 	}
 }
